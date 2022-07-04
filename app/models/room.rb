d57564a55093188ac1079abb7c6eabef0ddc0754
dominguesgabe <gabrieldomingues522@gmail.com>
2022-07-04 16:40:26 -0300

    iniciada traducao do app com i18n

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
+	validates_presence_of :title, :location
+	validates :description, :length => {:minimum => 30}
+	
 	def complete_name
 		"#{title}, #{location}"
 	end
